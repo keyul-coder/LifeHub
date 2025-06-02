cf62e5700d40ad985c38aaa55212b90da801166c
jshah244102 <j_shah244102@fanshaweonline.ca>
2025-06-02 17:49:39 -0400

    Basic Code Stucture

---
+++ LifeHub/LifeHub/Controller/HomeVC.swift
@@ -0,0 +1,24 @@
+//
+//  HomeVC.swift
+//  LifeHub
+//
+//  Created by Jenish Shah on 2025-06-02.
+//
+
+/// HomeVC
+class HomeVC: ParentVC {
+    
+    /// View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.prepareUI()
+    }
+}
+
+// MARK: - UI Related Method(s)
+extension HomeVC {
+
+    func prepareUI() {
+        
+    }
+}
---
+++ LifeHub/LifeHub/Controller/ParentVC.swift
@@ -0,0 +1,30 @@
+//
+//  ParentVC.swift
+//  LifeHub
+//
+//  Created by Jenish Shah on 2025-06-02.
+//
+
+import UIKit
+
+/// ParentVC
+class ParentVC: UIViewController {
+    
+    /// @IBOutlet
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    /// View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.parentPrepareUI()
+    }
+}
+
+// MARK: - UI Related Method(s)
+extension ParentVC {
+    
+    func parentPrepareUI() {
+        print(self.classForCoder)
+    }
+}
