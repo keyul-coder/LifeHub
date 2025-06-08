f7d0047000af1b3da91b66b327d029343ff002ce
jshah244102 <j_shah244102@fanshaweonline.ca>
2025-06-07 21:41:20 -0400

    HomeVC added .

--- LifeHub/LifeHub/OtherClasses/UIViewExtension.swift
+++ LifeHub/LifeHub/OtherClasses/UIViewExtension.swift
@@ -28,4 +28,49 @@ extension UIView {
             self.cornerRadius = newValue ? self.frame.height / 2 : self.cornerRadius
         }
     }
+    
+    @IBInspectable var shadowColor: UIColor {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor ?? UIColor.black.cgColor)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: self.layer.borderColor ?? UIColor.black.cgColor)
+        }
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
 }
